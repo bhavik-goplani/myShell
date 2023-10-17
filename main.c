7d9a364e5366418226d8a50b2c73d29f2ca5cab9
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-17 18:37:33 -0500

    tokenization

--- main.c
+++ main.c
@@ -2,21 +2,53 @@
 
 int main(int argc, char **argv) {
     char *prompt = "[QUASH]$ ";
-    char *lineptr;
+    char *lineptr = NULL, *lineptr_copy = NULL;
+    const char *delimiter = " \n";
+    char *token = NULL;
     size_t n = 0;
     ssize_t read = 0;
+    int num_tokens = 0;
+    int i;
 
     (void)argc;
-    (void)argv;
     printf("Wekcome...\n");
-    
+
     while (read != -1) {
         printf("%s", prompt);
         read = getline(&lineptr, &n, stdin);
+        
+        lineptr_copy = malloc(sizeof(char) * read);
+        strcpy(lineptr_copy, lineptr);
+
+        token = strtok(lineptr, delimiter);
+        
+        while (token != NULL) {
+            num_tokens++;
+            token = strtok(NULL, delimiter);
+        }
+        num_tokens++;
+
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        token = strtok(lineptr_copy, delimiter);
+
+        for (i = 0; token!= NULL; i++) {
+            argv[i] = malloc(sizeof(char) * strlen(token));
+            strcpy(argv[i], token);
+            token = strtok(NULL, delimiter);
+        }
+        argv[i] = NULL;
+
         printf("%s\n", lineptr);
+
+        for (i = 0; argv[i] != NULL; i++) {
+            printf("%s\n", argv[i]);
+        }
+        free(lineptr);
+        free(lineptr_copy);
+        free(argv);
     }
 
     
-    free(lineptr);
     return (0);
 }
