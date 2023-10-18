cfe9b9a22305eff89890078dfaae42e1a62fe1d3
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-18 17:10:22 -0500

    fix

--- get_location.c
+++ get_location.c
@@ -14,22 +14,23 @@ char *get_location(char *command) {
         token = strtok(path_copy, ":");
 
         while(token != NULL) {
-                token_len = strlen(token);
-                file_path = malloc(sizeof(char) * (token_len + command_len + 2));
+            token_len = strlen(token);
+            file_path = malloc(token_len + command_len + 2);
 
-                strcpy(file_path, token);
-                strcat(file_path, "/"); 
-                strcat(file_path, command);
-                strcat(file_path, "\0"); 
+            strcpy(file_path, token);
+            strcat(file_path, "/"); 
+            strcat(file_path, command);
+            strcat(file_path, "\0"); 
 
-                if (stat(file_path, &buffer) == 0) {
-                    free(path_copy);
-                    return (file_path);
-                }
-                else {
-                    free(file_path);
-                    file_path = NULL;
-                }
+            if (stat(file_path, &buffer) == 0) {
+                free(path_copy);
+                return (file_path);
+            }
+            else {
+                free(file_path);
+                file_path = NULL;
+                token = strtok(NULL, ":");
+            }
         }
         free(path_copy);
 
