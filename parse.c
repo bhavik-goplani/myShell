e3b0b268d38c938465a6bd205bf01fb72b1a18bf
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-19 19:19:48 -0500

    refactor parsing

---
+++ parse.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+char **parse_command(char *lineptr, char **argv) {
+    char lineptr_copy = NULL, token = NULL;
+    const char *delimiter = " \n";
+    int num_tokens = 0;
+
+    lineptr_copy = strdup(lineptr);
+
+    token = strtok(lineptr, delimiter);
+    
+    while (token != NULL) {
+        num_tokens++;
+        token = strtok(NULL, delimiter);
+    }
+    argv = malloc(sizeof(char *) * (num_tokens+1));
+
+    num_tokens = 0;
+    token = strtok(lineptr_copy, delimiter);
+    while (token != NULL) {
+        argv[num_tokens] = strdup(token);
+        num_tokens++;
+        token = strtok(NULL, delimiter);
+    }
+    argv[num_tokens] = NULL;
+
+    free(lineptr);
+    free(lineptr_copy);
+    
+    return (argv);
+}
