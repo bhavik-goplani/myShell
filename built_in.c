a85e3add6f0cd9783aef192186bcb75e905d3e2a
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2023-10-21 17:53:57 -0500

    export function

--- built_in.c
+++ built_in.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 char *builtin_str[] = {
-  "cd",
-  "exit",
-  "quit"
+    "cd",
+    "exit",
+    "quit",
+    "export"
 };
 
 int (*builtin_func[]) (char **) = {
-  &sh_cd,
-  &sh_exit,
-  &sh_exit
+    &sh_cd,
+    &sh_exit,
+    &sh_exit,
+    &sh_export
 };
 
 int sh_num_builtins() {
@@ -30,6 +32,34 @@ int sh_cd(char **argv)
         else {
             if (chdir(argv[1]) == -1) {
                 perror("ErrorChangingDirectory:");
+            }
+        }
+        return (0);
+    }
+    return (1);
+}
+
+int sh_export(char **argv)
+{
+    if (strcmp(argv[0], "export") == 0) {
+        if (argv[1] == NULL) {
+            printf("No arguments given to export\n");
+        }
+        else {
+
+            for (int i = 1; argv[i]!= NULL; i++) {
+                char *name = strtok(argv[i], "=");
+                char *value = strtok(NULL, "=");
+
+                if (name == NULL || value == NULL) {
+                    printf("Invalid argument given to export\n");
+                    return (0);
+                }
+                else {
+                    if (setenv(name, value, 1) == -1) {
+                        perror("ErrorExportingVariable:");
+                    }
+                }
             }
         }
         return (0);
